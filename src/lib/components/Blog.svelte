ba779c2fcbe1d8b1ff7ea0f23f403b1e70f2b4fa
Raine <160113821+yorubanashi@users.noreply.github.com>
2024-08-07 19:00:05 -0600

    Starting to prototype nonsense

---
+++ src/lib/components/Blog.svelte
@@ -0,0 +1,20 @@
+<div id="blog">
+  <aside id="menu">a</aside>
+  <div id="page">b</div>
+</div>
+
+<style>
+  #blog {
+    width: 76rem;
+    margin: 0 auto;
+    display: flex;
+  }
+
+  #menu {
+    flex-basis: 16rem;
+  }
+
+  #page {
+    flex-grow: 1;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,5 @@
 <script lang="ts">
-  import type { Link } from "$lib/components/Navigation.svelte";
-  import Navigation from "$lib/components/Navigation.svelte";
-
-  const LINKS: Link[] = [
-    { link: "/chinese", label: "中文" },
-    { link: "/#", label: "日本語 [WIP]" },
-  ];
+  import Blog from "$lib/components/Blog.svelte";
 </script>
 
-<Navigation links={LINKS} />
+<Blog />
